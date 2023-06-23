95372e1672299885f639cefb9bec202d3139dcc8
Charles Trangay <charlestrangay@Charless-MacBook-Air.local>
2023-06-22 20:40:55 -0400

    changed cards style

--- src/components/CustomCard.tsx
+++ src/components/CustomCard.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
-import { Card, CardActionArea, CardMedia, CardContent } from "@mui/material";
 import CustomButton from "../components/CustomButton";
+import Image from "next/image";
 
 interface CustomCardProps {
   image: string;
@@ -15,32 +15,31 @@ interface CustomCardProps {
 const CustomCard: FC<CustomCardProps> = (props) => {
   const { image, alt, title, description, hrefWebsite, hrefCode } = props;
   return (
-    <Card
-      className="lg:max-w-xl md:max-w-xl max-w-lg min-h-full ease-out transition duration-1000 hover:duration-300 hover:-translate-y-3"
-      elevation={10}
-      key = {props.id}
-    >
-      <CardActionArea disableTouchRipple>
-        <CardMedia
-          component="img"
-          image={image}
+    <div className="card card-compact bg-base-100 shadow-xl lg:max-w-xl md:max-w-xl max-w-lg min-h-full ease-out transition duration-1000 hover:duration-300 hover:-translate-y-3">
+      <figure>
+        <Image
+          src={image}
+          width={600}
+          height={600}
           alt={alt}
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          priority={true}
           className="object-cover object-top md:max-h-40 max-h-36"
         />
-        <CardContent>
-          <h1 className="text-gray-700 font-semibold mb-1 lg:mb-2 2xl:text-4xl xl:text-3xl lg:text-2xl md:text-xl sm:text-lg text-md">
-            {title}
-          </h1>
-          <p className="text-gray-600 leading-relaxed 2xl:text-2xl xl:text-xl lg:text-lg md:text-lg sm:text-sm text-xs">
-            {description}
-          </p>
-        </CardContent>
-        <div className="flex justify-center md:mb-3 mb-2 md:gap-2 gap-1">
+      </figure>
+      <div className="card-body">
+        <h2 className=" text-gray-700 font-semibold mb-1 lg:mb-2 2xl:text-4xl xl:text-3xl lg:text-2xl md:text-xl sm:text-lg text-md">
+          {title}
+        </h2>
+        <p className="text-gray-600 leading-relaxed 2xl:text-2xl xl:text-xl lg:text-lg md:text-lg sm:text-sm text-xs">
+          {description}
+        </p>
+        <div className="card-actions flex justify-center md:mb-2 mb-1 md:gap-2 gap-1">
           <CustomButton href={hrefWebsite} target="_blank" text="Website" />
           <CustomButton href={hrefCode} target="_blank" text="Code" />
         </div>
-      </CardActionArea>
-    </Card>
+      </div>
+    </div>
   );
 };
 
