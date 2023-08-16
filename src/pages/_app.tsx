00d4eecc67a9c52bbaf03f3b303183008f5e3d79
Charles <charles.trangay11@gmail.com>
2023-08-15 23:22:58 -0400

    added meta data for seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,8 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import FooterSection from "@/components/layouts/FooterSection";
 import NavbarSection from "@/components/layouts/NavbarSection";
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from "@vercel/analytics/react";
+import Head from "next/head";
 // Fonts
 import { Rubik } from "next/font/google";
 const rubik = Rubik({ subsets: ["latin"] });
@@ -12,6 +13,10 @@ const rubik = Rubik({ subsets: ["latin"] });
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Charles Trangay</title>
+        <meta name="description" content="Software Engineer | My portfolio" />
+      </Head>
       <NavbarSection />
       <main className={rubik.className}>
         <Component {...pageProps} />
