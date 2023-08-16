4d5640efa24728768a712e75df5e786c6653216b
Charles <charles.trangay11@gmail.com>
2023-08-15 23:29:07 -0400

    fixed footer color

--- src/components/layouts/FooterSection.tsx
+++ src/components/layouts/FooterSection.tsx
@@ -3,7 +3,7 @@ interface FooterProps {}
 
 const Footer: FC<FooterProps> = ({}) => {
   return (
-    <footer className="bg-bg_color py-2 text-paragraph_color md:py-4">
+    <footer className="bg-bg_white py-2 text-paragraph_color md:py-4">
       <p className="mt-1 text-center text-sm opacity-50 md:mt-2 md:text-lg">
         &copy; {new Date().getFullYear()} Charles Trangay. All rights reserved.
       </p>
