07d2b195b6017c52696a69d797c606e3b3e455fc
Charles <charles.trangay11@gmail.com>
2023-08-17 00:27:20 -0400

    added email icon on contact section

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
+import { AiFillGithub, AiFillLinkedin, AiFillMail } from "react-icons/ai";
 import { ContactTitle, ContactIcon } from "@/components/ui/Contact";
 
 interface ContactProps {}
@@ -7,21 +7,29 @@ interface ContactProps {}
 const Contact: FC<ContactProps> = ({}) => {
   return (
     <section className="bg-bg_white">
-    <div className="mx-auto w-11/12 flex flex-col items-center py-6 md:py-12" id="contact">
-      <ContactTitle className="text-center">On the web</ContactTitle>
-      <div className="mt-2 flex gap-2 md:mt-6 md:gap-3">
-        <ContactIcon
-          href="https://www.linkedin.com/in/charles-trangay/"
-          icon={<AiFillLinkedin />}
-          tooltip="LinkedIn"
-        />
-        <ContactIcon
-          href="https://github.com/Borality"
-          icon={<AiFillGithub />}
-          tooltip="Github"
-        />
+      <div
+        className="mx-auto flex w-11/12 flex-col items-center py-6 md:py-12"
+        id="contact"
+      >
+        <ContactTitle className="text-center">On the web</ContactTitle>
+        <div className="mt-2 flex gap-2 md:mt-6 md:gap-3">
+          <ContactIcon
+            href="https://www.linkedin.com/in/charles-trangay/"
+            icon={<AiFillLinkedin />}
+            tooltip="LinkedIn"
+          />
+          <ContactIcon
+            href="https://github.com/Borality"
+            icon={<AiFillGithub />}
+            tooltip="Github"
+          />
+          <ContactIcon
+            href="mailto:charles-trangay-portfolio-contact.overfeed094@aleeas.com"
+            icon={<AiFillMail />}
+            tooltip="Email"
+          />
+        </div>
       </div>
-    </div>
     </section>
   );
 };
