6e86d6ebac86907e8e292deb9241c74456e1a92e
Charles <charles.trangay11@gmail.com>
2025-10-26 02:32:57 -0400

    fix for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -20,5 +20,5 @@ export default defineConfig(({ mode }) => ({
 			"@": path.resolve(__dirname, "./src"),
 		},
 	},
-	base: mode === "production" ? "/portfolio/" : "/",
+	base: mode === "production" ? "/" : "/",
 }));
